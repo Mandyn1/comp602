ca58193c12d15ac79c414593bc4485af92ee6fc3
Becky Zhou <chefqiz@gmail.com>
2025-09-26 01:09:47 +1200

    All features in Car Theft Scene are completed and working properly

--- RamRaid Simulator/Assets/Scripts/CarMenu.cs
+++ RamRaid Simulator/Assets/Scripts/CarMenu.cs
@@ -1,19 +1,45 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class CarMenu : MonoBehaviour
 {
     [SerializeField] private GameObject carMenuUI;
+    [SerializeField] private GameObject carInfoPanel;
+    [SerializeField] private TMP_Text carInfoText;
+
+    private string currentCarInfo;
+
+    public void SetCar(string info)
+    {
+        currentCarInfo = info;
+    }
 
     public void StealCar()
     {
         carMenuUI.SetActive(false);
 
+        //Charles said he will handle the scene change
+        //SceneManager.LoadScene("MiniGame_FollowMap");
+
     }
     public void Inspect() //not sure if needed
     {
-        carMenuUI.SetActive(false);
+        if (!string.IsNullOrEmpty(currentCarInfo))
+        {
+            carInfoPanel.SetActive(true);
+            carInfoText.text = currentCarInfo;  
+            
+        }
 
     }
+
+    public void CloseInfoPanel()
+    {
+        carInfoPanel.SetActive(false);
+    }
+
     public void Cancel()
     {
         carMenuUI.SetActive(false);
--- RamRaid Simulator/Assets/Scripts/CarTrigger.cs
+++ RamRaid Simulator/Assets/Scripts/CarTrigger.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class CarTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject carMenuUI;
+    [SerializeField] private CarMenu carMenu;        // reference to CarMenu script
+    [TextArea] public string carInfo;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) //if the player is near car menu activate
         {
             carMenuUI.SetActive(true);
-
+            carMenu.SetCar(carInfo);
         }
     }
 }
