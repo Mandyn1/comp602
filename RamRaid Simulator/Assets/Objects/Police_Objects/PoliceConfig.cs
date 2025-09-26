6f57528c9c800010d6e67a37775604033b1781f6
love <love@loves-MacBook-Pro.local>
2025-09-27 04:26:32 +1200

    cleanup - removedduplicate police scripts from objects/police_objects

--- RamRaid Simulator/Assets/Objects/Police_Objects/PoliceConfig.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Game/Police Config")]
-public class PoliceConfig : ScriptableObject
-{
-    public int maxUnits = 3;     // how many police units can be placed
-    public int startBudget = 100; // how much money police start with
-}
