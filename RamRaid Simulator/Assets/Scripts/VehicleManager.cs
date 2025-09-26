c7fc8dbfb9edf3219977a14a9455cd20e656095c
Becky Zhou <chefqiz@gmail.com>
2025-09-26 23:47:35 +1200

    Added mini game object to car theft

--- RamRaid Simulator/Assets/Scripts/VehicleManager.cs
+++ RamRaid Simulator/Assets/Scripts/VehicleManager.cs
@@ -2,51 +2,17 @@
 
 public class VehicleManager : MonoBehaviour
 {
-    [Header("Motorcycle")]
-    [SerializeField] private GameObject motorcycle; // assign prefab in Inspector
-    [SerializeField] private Transform spawnPoint; // assign empty Transform in Inspector
+    public GameObject car;     
+    public GameObject motorbike; 
+    public Transform spawnPoint; //vehicle spawns
 
-    public void CompleteTheft(GameObject theftPlayer, GameObject targetVehicle)
+    void Start()
     {
-        Debug.Log("Car theft successful! Player can now driving.");
+        string vehicle = PlayerPrefs.GetString("Vehicle", "Motorbike");
 
-        // Disable player
-        var playerMove = theftPlayer.GetComponent<PlayerMovement>();
-        if (playerMove != null) playerMove.enabled = false;
-        theftPlayer.SetActive(false);
-
-        //enable car movement
-        var carMove = targetVehicle.GetComponent<CarMovement>();
-        if (carMove != null) carMove.enabled = true;
-
-    }
-
-    public void FailTheft(GameObject theftPlayer)
-    {
-        Debug.Log("Car theft failed - get a motorbike instead");
-
-        // disable player
-        var playerMove = theftPlayer.GetComponent<PlayerMovement>();
-        if (playerMove != null) playerMove.enabled = false;
-        theftPlayer.SetActive(false);
-
-        // spawn motorcycle
-        if (motorcycle != null && spawnPoint != null)
-        {
-            GameObject moto = Instantiate(motorcycle, spawnPoint.position, Quaternion.identity);
-
-            var motoMove = moto.GetComponent<CarMovement>();
-            if (motoMove != null)
-            {
-                motoMove.enabled = true;
-                motoMove.moveSpeed = 3f; // slower than car
-            }
-
-            // CarExitManager can also be used for motorcycles
-        }
+        if (vehicle == "Car")
+            Instantiate(car, spawnPoint.position, Quaternion.identity);
         else
-        {
-            Debug.LogWarning("motorcycle prefab or spawn point not assigned");
-        }
+            Instantiate(motorbike, spawnPoint.position, Quaternion.identity);
     }
 }
