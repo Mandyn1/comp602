f9ef031bec4ddb1f156ee7483cc33dbfd4de8e29
Mandyn1 <88999275+Mandyn1@users.noreply.github.com>
2025-09-26 23:38:52 +1200

    Recovering Josh_Core
    Josh_Core had some weird stuff happen and is now broken, this contains the unique files from that branch

--- RamRaid Simulator/Assets/Scripts/Data&Core/PlayerData.cs
+++ RamRaid Simulator/Assets/Scripts/Data&Core/PlayerData.cs
@@ -4,10 +4,14 @@ using Photon.Realtime;
 
 public class PlayerData : MonoBehaviour
 {
-
-    public PhotonView photonView;
     public Player player1;
     public Player player2;
+
+    public int player1Score;
+    public int player2Score;
+
+    public string currentRaidLocation;
+    public float policeResponseTime;
 
     void Awake()
     {
@@ -17,7 +21,23 @@ public class PlayerData : MonoBehaviour
 
     public void GetPlayers()
     {
-        player1 = PhotonNetwork.CurrentRoom.GetPlayer(0);
-        player2 = PhotonNetwork.CurrentRoom.GetPlayer(1);
+        player1 = PhotonNetwork.CurrentRoom.Players[0];
+        player2 = PhotonNetwork.CurrentRoom.Players[1];
+    }
+
+    public Player SendPlayer(int playerNumber)
+    {
+
+        if (player1 == null) GetPlayers();
+
+        switch (playerNumber)
+        {
+            case 1:
+                return player1;
+            case 2:
+                return player2;
+            default:
+                return null;
+        }
     }
 }
