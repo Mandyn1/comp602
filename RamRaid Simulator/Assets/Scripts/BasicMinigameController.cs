6299b353d0f74e0f3109977a302753950c7f356e
CharlesThaugland <chazzafpv@gmail.com>
2025-09-26 20:23:26 +1200

    added minigame for stealing item, created trigger in stealable item

--- RamRaid Simulator/Assets/Scripts/BasicMinigameController.cs
+++ RamRaid Simulator/Assets/Scripts/BasicMinigameController.cs
@@ -3,14 +3,26 @@ using UnityEngine;
 
 public class BasicMinigameController : MonoBehaviour
 {
-    //TODO
-    //[SerializeField] newScene;
-    SceneTransition sceneTrans;
+    public GameObject playerObj;
+    public GameObject menuCanvas;
+    private PlayerMovement player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //set closed by default
+        menuCanvas.SetActive(false);
+        if (menuCanvas == null)
+        {
+            Debug.LogError("Menu Canvas is not assigned!");
+        }
 
+        //get pl;aye to freeze movement
+        player = playerObj.GetComponent<PlayerMovement>();
+        if (player == null)
+        {
+            Debug.LogError("PlayerMovement component not found on player object!");
+        }
     }
 
     // Update is called once per frame
@@ -21,13 +33,31 @@ public class BasicMinigameController : MonoBehaviour
 
     public void Steal()
     {
-        //player chooses to steal, so gains a point 
+        //player chooses to steal, so gains a point
+        //TODO: Yahya works in this section
+        Debug.Log("player chooses to steal!!!");
+        this.Close();
     }
 
-    public void DontSteal()
+    public void Leave()
     {
         //player chooses to not steal so do not gain point
-        sceneTrans.tr
+        //TODO: Yahya works in this section
+        Debug.Log("player chooses to not steal");
+        this.Close();
+    }
 
+    public void Open()
+    {
+        //open the menu and freeze player
+        menuCanvas.SetActive(true);
+        player.Freeze();
+    }
+
+    public void Close()
+    {
+        //open the menu and unfreeze player
+        menuCanvas.SetActive(false);
+        player.UnFreeze();
     }
 }
--- RamRaid Simulator/Assets/Scripts/PlayerMovement.cs
+++ RamRaid Simulator/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,9 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     //variables
     [SerializeField] private float moveSpeed = 5f;
+    private Boolean freeze = false;
     private Rigidbody2D rb;
     private Vector2 moveInput;
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,10 +19,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = moveInput * moveSpeed;
+        //update players position
+        if (!freeze)
+        {
+            rb.linearVelocity = moveInput * moveSpeed;
+        }
     }
 
-    public void Move(InputAction.CallbackContext context){
-        moveInput = context.ReadValue<Vector2>();
+    public void Move(InputAction.CallbackContext context)
+    {
+        //move
+        if (!freeze)
+        {
+            moveInput = context.ReadValue<Vector2>();
+        }
+    }
+
+    public void Freeze()
+    {
+        //freeze players movement
+        freeze = true;
+    }
+
+    public void UnFreeze()
+    {
+        //unfreezes players movement
+        freeze = false;
     }
 }
