a8f4f85ad9f679f2b4e12d0bf2fee130e671ae01
CharlesThaugland <chazzafpv@gmail.com>
2025-09-26 17:48:56 +1200

    basic mini game scene created with UI components, yet to make manager script so game is functional

---
+++ RamRaid Simulator/Assets/Scripts/BasicMinigameController.cs
@@ -0,0 +1,33 @@
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class BasicMinigameController : MonoBehaviour
+{
+    //TODO
+    //[SerializeField] newScene;
+    SceneTransition sceneTrans;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Steal()
+    {
+        //player chooses to steal, so gains a point 
+    }
+
+    public void DontSteal()
+    {
+        //player chooses to not steal so do not gain point
+        sceneTrans.tr
+
+    }
+}
