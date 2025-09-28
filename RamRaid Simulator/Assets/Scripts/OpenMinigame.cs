05efd51018761748b86303361205dcbe068d1535
Mandyn1 <88999275+Mandyn1@users.noreply.github.com>
2025-09-28 14:25:43 +1300

    Altered OpenMinigame script
    Moved segments into active-scene-related if-statements to avoid breaking scenes made by other team members

--- RamRaid Simulator/Assets/Scripts/OpenMinigame.cs
+++ RamRaid Simulator/Assets/Scripts/OpenMinigame.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OpenMinigame : MonoBehaviour
 {
@@ -13,10 +14,14 @@ public class OpenMinigame : MonoBehaviour
         controller = game ? game.GetComponent<BasicMinigameController>() : null;
         if (controller == null) Debug.LogError("BasicMinigameController not found on 'game' object!");
 
-        // Support both same-object and child setup
-        lootable = GetComponent<LootableItem>();
-        if (lootable == null) lootable = GetComponentInChildren<LootableItem>(true);
-        if (lootable == null) Debug.LogError("LootableItem component not found on this item or its children!");
+        // Select car scene doesnt have a lootable item of this type - josh
+        if (SceneManager.GetActiveScene().name != "SelectCarScene")
+        {
+            // Support both same-object and child setup
+            lootable = GetComponent<LootableItem>();
+            if (lootable == null) lootable = GetComponentInChildren<LootableItem>(true);
+            if (lootable == null) Debug.LogError("LootableItem component not found on this item or its children!");
+        }
     }
 
     // Update is called once per frame
@@ -30,15 +35,19 @@ public class OpenMinigame : MonoBehaviour
         //check if the player collides with the collision box
         if (!collision.CompareTag("Player")) return;
 
-        var wallet = collision.GetComponent<PlayerWallet>();
-        if (wallet == null)
+        // select car scene doesnt have/need a player wallet - josh
+        if (SceneManager.GetActiveScene().name != "SelectCarScene")
         {
-            Debug.LogError("Player does not have a PlayerWallet component!");
-            return;
-        }
+            var wallet = collision.GetComponent<PlayerWallet>();
+            if (wallet == null)
+            {
+                Debug.LogError("Player does not have a PlayerWallet component!");
+                return;
+            }
 
-        controller.currentTargetItem = lootable;
-        controller.playerWallet = wallet;
+            controller.currentTargetItem = lootable;
+            controller.playerWallet = wallet;
+        }
 
         //set the minigame active and freeze players movement
         controller.Open();
