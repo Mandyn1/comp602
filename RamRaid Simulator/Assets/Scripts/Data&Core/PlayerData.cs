d6ed972128e3063edfd8e5474edcb0dabfdbab71
Mandyn1 <88999275+Mandyn1@users.noreply.github.com>
2025-10-16 22:51:33 +1300

    Started Stats Screen
    Started data gathering and syncing functions for end game stats screen.

--- RamRaid Simulator/Assets/Scripts/Data&Core/PlayerData.cs
+++ RamRaid Simulator/Assets/Scripts/Data&Core/PlayerData.cs
@@ -1,15 +1,56 @@
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using System.Globalization;
+using System.Collections.Generic;
+using System;
+using ExitGames.Client.Photon.StructWrapping;
 
 public class PlayerData : MonoBehaviour
 {
     public int score;
-    public int bank;
+    public int raiderBank;
+    public int policeBank;
     public string position;
 
-    public void ResetBank()
+    // For stats page
+    public int raiderMoneySpent;
+    public int policeMoneySpent;
+    public int carsStolen;
+    public float timeInRaid;
+    public int locationsLooted;
+    public int policeUnitsDeployed;
+    public float aveTimeToRaid;
+    public int raiderItemsBought;
+    public int raiderItemsUsed;
+    public int policeItemsBought;
+    public int policeItemsUsed;
+
+    public Dictionary<String, object> gatherData()
     {
-        bank = 0;
+        Dictionary<String, object> data = new Dictionary<string, object>
+        {
+            { "Score", score },
+            { "RaiderMoneyRemaining", raiderBank },
+            { "PoliceMoneyRemaining", policeBank },
+            { "RaiderMoneySpent", raiderMoneySpent },
+            { "PoliceMoneySpent", policeMoneySpent },
+            { "CarsStolen", carsStolen },
+            { "TimeInRaid", timeInRaid },
+            { "LocationsLooted", locationsLooted },
+            { "PoliceUnitsDeployed", policeUnitsDeployed },
+            { "AveTimeToRaid", aveTimeToRaid },
+            { "RaiderItemsBought", raiderItemsBought },
+            { "RaiderItemsUsed", raiderItemsUsed },
+            { "PoliceItemsBought", policeItemsBought },
+            { "PoliceItemsUsed", policeItemsUsed }
+        };
+
+        return data;
+    }
+
+    public void depositData(Dictionary<String, object> data)
+    {
+        raiderMoneySpent = (int) data["RaiderMoneySpent"];
     }
 }
