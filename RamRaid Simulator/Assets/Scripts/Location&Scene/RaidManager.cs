5a1e04da3000aca2ba469e5fb6a2254f40f3e658
CharlesThaugland <chazzafpv@gmail.com>
2025-10-24 17:08:17 +1300

    raid manage script added to handle raider being caught / not caught. Script manages the temporary player wallet and updates to live variables accordingly. Scene transition area script was modified to suport either indoor / outdoor updates

---
+++ RamRaid Simulator/Assets/Scripts/Location&Scene/RaidManager.cs
@@ -0,0 +1,82 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RaidManager : MonoBehaviour
+{
+    //variables
+    [Header("Raider and police interaction")]
+    public double policeDistanceFromRaid;
+    public int raidLocationNum;
+    public GameObject policePlacer;
+    private DistanceChecker distanceCheckerScript;
+    RaidSceneTransition sceneTransition;
+    public GameObject bounds;
+    public GameObject player;
+
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //get the distance script from the police car placer
+
+        if (policePlacer != null)
+        {
+            //assign the distance checker
+            distanceCheckerScript = policePlacer.GetComponentInChildren<DistanceChecker>();
+        }
+
+        //get the map scene transition object of from map bound children
+        sceneTransition = bounds.GetComponentInChildren<RaidSceneTransition>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update the distance of the police from the raid scene using switch to check location manager is assigned to
+        switch (raidLocationNum)
+        {
+            case 1:
+                policeDistanceFromRaid = distanceCheckerScript.location1Time;
+                break;
+            case 2:
+                policeDistanceFromRaid = distanceCheckerScript.location2Time;
+                break;
+            case 3:
+                policeDistanceFromRaid = distanceCheckerScript.location3Time;
+                break;
+            case 4:
+                policeDistanceFromRaid = distanceCheckerScript.location4Time;
+                break;
+            case -444: //FOR TESTING
+                policeDistanceFromRaid = 0;
+                break;
+            default:
+                policeDistanceFromRaid = -1; //invalid location has been used
+                break;
+        }
+
+        //now that the update distance has been assigned, check the distance
+        if (policeDistanceFromRaid <= 0)
+        {
+            //raider is to be caught
+            raiderCaught();
+        }
+    }
+
+    void raiderCaught()
+    {
+        //remove the waypoint, raider should not be able to exit 
+        if (bounds != null)
+        {
+            //get the script component, and set the waypoint to false
+            sceneTransition.wayPointActive = false;
+        }
+
+        //raider is caught, momey from wallet does not increment, remove the current earnings
+        PlayerWallet wallet = player.GetComponent<PlayerWallet>();
+        wallet.balance = 0;
+
+        return; //exit the loop, no more updates needed
+    }
+}
---
+++ RamRaid Simulator/Assets/Scripts/Location&Scene/RaidSceneTransitionArea.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RaidSceneTransition : MonoBehaviour
+{
+    //variables 
+    public GameObject transitionTo;
+    public GameObject currentScene;
+
+    public GameObject Player;
+    private PlayerWallet wallet;
+
+    [Header("Location")]
+    public bool wayPointActive = true;
+    public bool indoorLocation;
+    public bool outdoorLocation;
+
+    void Start()
+    {
+        //need to disable the parsed in scene first so no obverlap
+        if (transitionTo != null)
+        {
+            transitionTo.SetActive(false);
+        }
+        else
+        {
+            //prinmt debiug
+            Debug.Log("cannot set object as inacive (already null)");
+        }
+
+        //if the location is indoor, get the players wallet
+        if (indoorLocation)
+        {
+            wallet = Player.GetComponent<PlayerWallet>();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //check if the player collides with the collision box and input string scene is set
+        if (collision.gameObject.CompareTag("Player") && wayPointActive)
+        {
+            //enable the object that is being transitioned too
+            if (transitionTo != null)
+            {
+                //if the player is leaving the indoor location (fleeing, update the wallet to live variables)
+                if (indoorLocation)
+                {
+                    //update the players wallet
+                    GameObject gameManger = GameObject.Find("GameManager");
+                    
+                    //check if the game manager is null to be safe
+                    if(gameManger != null)
+                    {
+                        //now fetch the sate script to get the live 'raiderBank' variable
+                        GameState state = gameManger.GetComponent<GameState>();
+                        if(state != null)
+                        {
+                            //add the wallet balance to the raiders bank
+                            state.playerData[state.localPlayerNumber].raiderBank += wallet.balance;
+                        }
+                    }
+                    
+                    //log the output
+                    Debug.Log("Balance after Raid: " + wallet.balance);
+
+                }
+
+                transitionTo.SetActive(true); //enable transition area
+
+                //now hide the current object that is active
+                if (transitionTo.activeInHierarchy)
+                {
+                    currentScene.SetActive(false);
+                }
+
+                wayPointActive = false;
+
+            }
+            else
+            {
+                //give debug err
+                Debug.Log("Object to load is null");
+            }
+        }
+        else
+        {
+            if (!wayPointActive)
+            {
+                //user cannot exit because the waypoint has been deactiviated
+                Debug.Log("WARN!: waypoint deactivated");
+            }
+            else
+            {
+                //print err log (change to usefull error eventually)
+                Debug.Log("ERR!: wrong scene name");
+            }
+        }
+    }
+}
